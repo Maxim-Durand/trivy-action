2dd6ae431537960ba79f8b179a346e7ebbb41a05
Maxim-Durand <72691393+Maxim-Durand@users.noreply.github.com>
2024-01-20 15:02:58 -0500

    using sed to replace source_location in sbom output

--- entrypoint.sh
+++ entrypoint.sh
@@ -198,7 +198,8 @@ returnCode=$?
 set -e
 if [[ "${format}" == "github" ]]; then
   if [[ "$scanType" != "image" ]]; then
-      # TODO replace the manifest definition in the SBOM files by the docker image value
+    # TODO replace the manifest definition in the SBOM files by the docker image value
+    sed -i -e 's/"source_location": ".*"/"source_location": "${artifactRef}"/g' ./$(echo $output | xargs)
   fi  
   if [[ "$(echo $githubPAT | xargs)" != "" ]]; then
     printf "\n Uploading GitHub Dependency Snapshot"
